490364f2afa1ab6dbb2cdad2f3220fe2ab4ea02d
faiz1 <faiz1@NaviraFaisel.attlocal.net>
2018-12-30 06:54:12 -0500

    Added Data Provider Method to get values from testng.xml

--- src/main/java/RHTestNGMavenJenkins/RHMvnJnkns.java
+++ src/main/java/RHTestNGMavenJenkins/RHMvnJnkns.java
@@ -6,35 +6,55 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class RHMvnJnkns {
 	
 static WebDriver driver;
+String URLname, chromeDriver, chromeDriverPathSys, loginURL, loginLnk, username, password, loginClkButton;
+
+@Parameters({"URL", "Chromedriver", "ChromeDriverPath", "loginURL", "loginLink", "uname", "password", "loginClickButton"})
+@DataProvider
+public void getData(String URL, String chromedriver, String chromeDriverPath, String logURL, String loginLink, String uname, String pwd, String loginClickButton) {
+	URLname=URL;
+	chromeDriver=chromedriver;
+	chromeDriverPathSys=chromeDriverPath;
+	loginURL=logURL;
+	loginLnk=loginLink;
+	username=uname;
+	password=pwd;
+	loginClkButton=loginClickButton;
+}
 	
-@Parameters({"URL", "ChromeDriver", "ChromeDriverPath"})
+//@Parameters({"URL", "ChromeDriver", "ChromeDriverPath"})
 @BeforeClass
-	public void initializeBrowser(String URL, String chromeDriver, String chromeDriverPath) {
-		System.setProperty(chromeDriver, chromeDriverPath);
+	//public void initializeBrowser(String URL, String chromeDriver, String chromeDriverPath) {
+	public void initializeBrowser() {
+		System.out.println("Initialize Browser in progress...");
+		System.setProperty(chromeDriver, chromeDriverPathSys);
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
-		driver.get(URL);
+		driver.get(URLname);
+		System.out.println("Initialize Browser Completed");
 		//return driver;
 	}
 
-@Parameters({"loginURL", "loginLink", "username", "password", "loginClickButton"})
+//@Parameters({"loginURL", "loginLink", "username", "password", "loginClickButton"})
   @Test
-  public void login(String logURL, String loginLink, String username, String pwd, String loginClickButton) throws InterruptedException {
-	  if (driver.findElement(By.linkText(loginLink)).isDisplayed()) {
-			driver.findElement(By.linkText(loginLink)).click();
+  //public void login(String logURL, String loginLink, String username, String pwd, String loginClickButton) throws InterruptedException {
+  public void login() throws InterruptedException {
+	  System.out.println("Login in progress...");
+	  if (driver.findElement(By.linkText(loginLnk)).isDisplayed()) {
+			driver.findElement(By.linkText(loginLnk)).click();
 		}
 		String currentURL = driver.getCurrentUrl();
-		if (currentURL.equals(logURL)) {
+		if (currentURL.equals(loginURL)) {
 			Thread.sleep(500);
 			driver.findElement(By.name("username")).sendKeys(username);
-			driver.findElement(By.name("password")).sendKeys(pwd);
-			driver.findElement(By.className(loginClickButton)).click();
+			driver.findElement(By.name("password")).sendKeys(password);
+			driver.findElement(By.className(loginClkButton)).click();
 		}
 			Thread.sleep(2000);
 		if (driver.findElement(By.linkText("Home")).isDisplayed()) {
@@ -46,20 +66,25 @@ static WebDriver driver;
 	@Test(dependsOnMethods="login", priority=1)
 	public static void findStockValue() throws InterruptedException {
 		//CLDR
+		System.out.println("Find Stock Value in progress...");
 		Thread.sleep(2000);
 		String stockName1 = driver.findElement(By.xpath("//*[@id=\"react_root\"]/div/main/div[2]/div/div[1]/div/div/div[2]/div/div[1]/div/div/section[1]/a[1]/div[1]/h4")).getText();
 		//System.out.println(stockName1);
 		System.out.println("Value of " + stockName1 + " " + driver.findElement(By.xpath("//*[@id=\"react_root\"]/div/main/div[2]/div/div[1]/div/div/div[2]/div/div[1]/div/div/section[1]/a[1]/div[1]/div")).getText() + " stocks is: " + driver.findElement(By.xpath("//*[@id=\"react_root\"]/div/main/div[2]/div/div[1]/div/div/div[2]/div/div[1]/div/div/section[1]/a/h3")).getText());
+		System.out.println("Find Stock Value Completed");
 	}
 	
 	@Test(dependsOnMethods="login", priority=2)
 	public static void findCurrentBuyingPower() {
+		System.out.println("Find Current Buying Power in progress...");
 	driver.findElement(By.linkText("Account")).click();
 	System.out.println("Current Buying Power: " +driver.findElement(By.xpath("//*[@id=\"react_root\"]/div/main/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div[2]/div/header/div/div[2]/div/div[1]/h3")).getText());
+	System.out.println("Find Current Buying Power Completed");
 	}
   
   @AfterClass
   public void closeBrowser() {
 	  driver.close();
+	  System.out.println("Closing Browser...");
   }
 }
